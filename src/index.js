a678895fb25281f6cd67d99670de93b80d122c91
Julio Carvalho <juliocarvalhocbq@hotmail.com>
2020-03-01 19:10:51 -0300

    Componentes: Pai e Filho 1

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import Saudacao from './componentes/Saudacao'
+import Pai from './componentes/Pai'
 
 ReactDOM.render(
 <div>  
-   <Saudacao tipo="Bom dia" nome="João" />  
+   <Pai nome="Paulo" sobrenome="João" />  
 </div>
 , document.getElementById('root'))
 

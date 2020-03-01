00949b5acfa4cc8cc79e0d00512fbe184030941a
Julio Carvalho <juliocarvalhocbq@hotmail.com>
2020-03-01 18:40:00 -0300

    Componente de classe #1

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import Multi from './componentes/Multiplos'
+import Saudacao from './componentes/Saudacao'
 
 ReactDOM.render(
 <div>  
-    <Multi.BoaTarde nome="Ana"/>
-    <Multi.BoaNoite nome="Bia"/>
+   <Saudacao tipo="Bom dia" nome="João" />  
 </div>
 , document.getElementById('root'))
 
